27ecf6f1adfa7def18dd1dc224340dc7d9ad4fac
“Ayushi <AGajpal@netlink.com>
2023-05-21 13:14:13 +0530

    ok

--- src/services/article.js
+++ src/services/article.js
@@ -3,23 +3,32 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
 export const articleApi = createApi({
+
+    // 1
     reducerPath: 'articleApi',
+
+
+    // 3
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
         prepareHeaders: (headers) => {
             headers.set('X-RapidAPI-Key', rapidApiKey);
             headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
-
             return headers;
         },
     }),
+
+
+    // 2
     endpoints: (builder) => ({
         getSummary: builder.query({
             // encodeURIComponent() function encodes special characters that may be present in the parameter values
             // If we do not properly encode these characters, they can be misinterpreted by the server and cause errors or unexpected behavior. Thus that RTK bug
+            //whenever we are using user generated url, use encodeURIComponent
             query: (params) => `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
         }),
     }),
 })
 
+//fire the hook on demand
 export const { useLazyGetSummaryQuery } = articleApi
--- src/services/store.js
+++ src/services/store.js
@@ -2,9 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { articleApi } from "./article";
 
 export const store = configureStore({
+    //slice of a cake
+
     reducer : {
         [articleApi.reducerPath] : articleApi.reducer
     },
+
+    
     middleware:(getDefaultMiddleware)=> 
     getDefaultMiddleware().concat(articleApi.middleware)
 })
